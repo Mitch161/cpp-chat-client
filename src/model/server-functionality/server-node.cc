87ba24119fb4f117b0458c62783793d93c5b9d73
mitch161 <mitchellhardie1@gmail.com>
2021-09-19 12:05:17 +0100

    add faked getter method for server name

---
+++ src/model/server-functionality/server-node.cc
@@ -0,0 +1,7 @@
+#include "server-node.h"
+
+#include <string>
+
+using namespace model_server_functionality;
+
+std::string ServerNode::GetName() { return "test server"; }
---
+++ src/model/server-functionality/server-node.h
@@ -0,0 +1,13 @@
+#ifndef MODEL_SERVER_FUNCTIONALITY_SERVER_NODE_H_
+#define MODEL_SERVER_FUNCTIONALITY_SERVER_NODE_H_
+
+#include <string>
+
+namespace model_server_functionality {
+class ServerNode {
+ public:
+  std::string GetName();
+};
+}  // namespace model_server_functionality
+
+#endif
