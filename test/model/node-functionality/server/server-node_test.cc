8ff60d033894ba16ee4a3120e1d32b6ddbd864e9
mitch <mitchellhardie1@gmail.com>
2022-05-22 16:38:55 +0100

    update tests to match rearranged constructor params

---
+++ test/model/node-functionality/server/server-node_test.cc
@@ -0,0 +1,173 @@
+#include "model/node-functionality/server/server-node.h"
+
+#include <gtest/gtest.h>
+
+#include <string>
+
+using namespace model_node_functionality_server;
+
+class ServerNodeTest : public ::testing::Test {
+ public:
+  ServerNodeTest() {
+    uuid_one = "abcd1234";
+    uuid_two = "abcd5678";
+
+    name_one = "test server";
+    name_two = "micky server";
+
+    owner_one = "mitch";
+    owner_two = "micky";
+
+    server_one = new ServerNode(uuid_one, name_one, owner_one);
+    server_two = new ServerNode(uuid_two, name_two, owner_two);
+  }
+
+ protected:
+  std::string uuid_one;
+  std::string uuid_two;
+
+  std::string name_one;
+  std::string name_two;
+
+  std::string owner_one;
+  std::string owner_two;
+
+  ServerNode *server_one;
+  ServerNode *server_two;
+};
+
+TEST_F(ServerNodeTest, GetUuidTest) {
+  EXPECT_EQ(server_one->GetUuid(), uuid_one);
+}
+
+TEST_F(ServerNodeTest, GetUuidDoubleTest) {
+  EXPECT_EQ(server_one->GetUuid(), uuid_one);
+  EXPECT_EQ(server_two->GetUuid(), uuid_two);
+}
+
+TEST_F(ServerNodeTest, GetUuidConstant) {
+  std::string uuid = "mitch";
+  ServerNode tmp_server(uuid, name_one, owner_two);
+
+  EXPECT_EQ(tmp_server.GetUuid(), uuid);
+
+  uuid = "bob";
+  EXPECT_NE(tmp_server.GetUuid(), uuid);
+
+  uuid = "mitch";
+  EXPECT_EQ(tmp_server.GetUuid(), uuid);
+}
+
+TEST_F(ServerNodeTest, GetUuidMany) {
+  ServerNode *server_array[20];
+
+  for (int index = 0; index < 20; ++index) {
+    std::string uuid = std::to_string(index);
+    server_array[index] = new ServerNode(uuid, name_one, owner_one);
+  }
+
+  for (int index = 0; index < 20; ++index) {
+    EXPECT_EQ(server_array[index]->GetUuid(), std::to_string(index));
+  }
+}
+
+TEST_F(ServerNodeTest, GetUuidNotEqual) {
+  EXPECT_NE(server_one->GetUuid(), server_two->GetUuid());
+}
+
+TEST_F(ServerNodeTest, GetUuidEqual) {
+  ServerNode *tmp_server = new ServerNode(uuid_one, name_one, owner_one);
+
+  EXPECT_EQ(server_one->GetUuid(), tmp_server->GetUuid());
+}
+
+TEST_F(ServerNodeTest, GetNameTest) {
+  EXPECT_EQ(server_one->GetName(), name_one);
+}
+
+TEST_F(ServerNodeTest, GetNameDoubleTest) {
+  EXPECT_EQ(server_one->GetName(), name_one);
+  EXPECT_EQ(server_two->GetName(), name_two);
+}
+
+TEST_F(ServerNodeTest, GetNameConstant) {
+  std::string name = "mitch";
+  ServerNode tmp_server(uuid_one, name, owner_two);
+
+  EXPECT_EQ(tmp_server.GetName(), name);
+
+  name = "bob";
+  EXPECT_NE(tmp_server.GetName(), name);
+
+  name = "mitch";
+  EXPECT_EQ(tmp_server.GetName(), name);
+}
+
+TEST_F(ServerNodeTest, GetNameMany) {
+  ServerNode *server_array[20];
+
+  for (int index = 0; index < 20; ++index) {
+    std::string name = std::to_string(index);
+    server_array[index] = new ServerNode(uuid_one, name, owner_one);
+  }
+
+  for (int index = 0; index < 20; ++index) {
+    EXPECT_EQ(server_array[index]->GetName(), std::to_string(index));
+  }
+}
+
+TEST_F(ServerNodeTest, GetNameNotEqual) {
+  EXPECT_NE(server_one->GetName(), server_two->GetName());
+}
+
+TEST_F(ServerNodeTest, GetNameEqual) {
+  ServerNode *tmp_server = new ServerNode(uuid_one, name_one, owner_one);
+
+  EXPECT_EQ(server_one->GetName(), tmp_server->GetName());
+}
+
+TEST_F(ServerNodeTest, GetOwner) {
+  std::string owner = "mitch";
+  EXPECT_EQ(server_one->GetOwner(), owner);
+}
+
+TEST_F(ServerNodeTest, GetOwnerDouble) {
+  EXPECT_EQ(server_one->GetOwner(), owner_one);
+  EXPECT_EQ(server_two->GetOwner(), owner_two);
+}
+
+TEST_F(ServerNodeTest, GetOwnerConstant) {
+  std::string owner = "mitch";
+  ServerNode tmp_server(uuid_one, name_one, owner);
+
+  EXPECT_EQ(tmp_server.GetOwner(), owner);
+
+  owner = "bob";
+  EXPECT_NE(tmp_server.GetOwner(), owner);
+
+  owner = "mitch";
+  EXPECT_EQ(tmp_server.GetOwner(), owner);
+}
+
+TEST_F(ServerNodeTest, GetOwnerMany) {
+  ServerNode *server_array[20];
+
+  for (int index = 0; index < 20; ++index) {
+    std::string owner = std::to_string(index);
+    server_array[index] = new ServerNode(uuid_one, name_one, owner);
+  }
+
+  for (int index = 0; index < 20; ++index) {
+    EXPECT_EQ(server_array[index]->GetOwner(), std::to_string(index));
+  }
+}
+
+TEST_F(ServerNodeTest, GetOwnerNotEqual) {
+  EXPECT_NE(server_one->GetOwner(), server_two->GetOwner());
+}
+
+TEST_F(ServerNodeTest, GetOwnerEqual) {
+  ServerNode *tmp_server = new ServerNode(uuid_one, name_one, owner_one);
+
+  EXPECT_EQ(server_one->GetOwner(), tmp_server->GetOwner());
+}
